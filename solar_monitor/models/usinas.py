f79fe2ad072b636ca4fc41f6d7d47599b6f5912f
Lucas Oliveira <lucasjf25@gmail.com>
2025-05-15 21:17:53 -0300

    Update usinas.py

--- solar_monitor/models/usinas.py
+++ solar_monitor/models/usinas.py
@@ -1,6 +1,8 @@
 #Tabelas do Banco de Dados
+#Tabelas do Banco de Dados
 from sqlalchemy import Column, Integer, String              # Importa classes do SQLAlchemy para definir colunas e tipos de dados
-from models.base import Base                               # Importa a base declarativa
+from sqlalchemy.orm import relationship                     # Importa a classe relationship do SQLAlchemy
+from models.base import Base                                # Importa a base declarativa
 
  #TABELA DE USINAS
 class Usina(Base):
@@ -8,3 +10,5 @@ class Usina(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String, index=True)
+
+    inversores = relationship("Inversor", back_populates="usina") # Uma usina tem vários inversores
